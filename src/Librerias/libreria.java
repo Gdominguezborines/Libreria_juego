59d2ff4949973de9f702b1dc373ecf1ef89452ce
VictorSaaPerez <vsaaperez@danielcastelao.org>
2021-03-17 17:14:38 +0100

    vuelvo a meter los metodos mi libreria

--- src/Librerias/libreria.java
+++ src/Librerias/libreria.java
@@ -1,54 +1,39 @@
 package Librerias;
 
+import java.util.Scanner;
+
 public class libreria {
 
-        /**
-         * Muestra el turno de cada jugador
-         *
-         * @param turno
-         */
+    static Scanner teclado = new Scanner(System.in);
 
+    //Insertamos una posicion de una matriz un simbolo concreto
+    public static void insertarEn(char[][] matriz, int fila, int columna, char simbolo){
 
-        public void mostrarTurnoActual(boolean turno) {
-            if (turno) {
-                System.out.println("Le toca alJugador 1");
-            } else {
-                System.out.println("Le toca al jugador 2");
+        matriz[fila][columna] = simbolo;
+
+    }
+    //pedimos un numero y lo devolvemos
+    public static int pedirInteger(String mensaje){
+        System.out.println(mensaje);
+        int numero = teclado.nextInt();
+        return numero;
+    }
+    //Rellenamos la matriz con un simbolo
+    public static void rellenarMatriz (char [][] matriz, char simbolo){
+
+        for(int i=0;i<matriz.length;i++){
+            for(int j=0;j<matriz.length;j++){
+                matriz[i][j]= simbolo;
             }
         }
-
-        /**
-         * Se valida la posicion insertada
-         *
-         * @param tablero
-         * @param fila
-         * @param columna
-         * @return
-         */
-
-
-        public static boolean validarPosicion(char[][] tablero, int fila, int columna) {
-
-            if (fila >= 0 && fila < tablero.length && columna >= 0 && columna < tablero.length) {
-                return true;
-            }
-            return false;
+    }
+    //Indicamos si en una posicion hay una marca
+    public static boolean hayValorPosicion(char[][] matriz, int fila, int columna, char simboloDef){
+        if(matriz[fila][columna] != simboloDef){
+            return true;
         }
-
-        /**
-         * Muestra la matriz
-         *
-         * @param matriz
-         */
-
-        public static void mostrarMatriz(char[][] matriz) {
-            for (int i = 0; i < matriz.length; i++) {
-                for (int j = 0; j < matriz[0].length; j++) {
-                    System.out.print(matriz[i][j] + " ");
-                }
-                System.out.println("");
-            }
-        }
+        return false;
+    }
 
 }
 
